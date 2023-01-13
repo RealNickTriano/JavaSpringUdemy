6b578eb10ec72efbaf273cfb3d9184b2b0a604d2
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2023-01-13 17:11:09 -0500

    Exploring Loose Coupling Level 2 - Spring Framework

--- learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/LearnSpringFrameworkApplication.java
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/LearnSpringFrameworkApplication.java
@@ -2,12 +2,34 @@ package com.in28minutes.spring.learnspringframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+import com.in28minutes.spring.learnspringframework.game.GameRunner;
+import com.in28minutes.spring.learnspringframework.game.GamingConsole;
+import com.in28minutes.spring.learnspringframework.game.MarioGame;
+import com.in28minutes.spring.learnspringframework.game.PacManGame;
+import com.in28minutes.spring.learnspringframework.game.SuperContraGame;
+
+@ComponentScan(basePackages = "com.in28minutes.spring.learnspringframework")
+
+
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		
+		ConfigurableApplicationContext context 
+			= SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		
+		//MarioGame game = new MarioGame();
+		//SuperContraGame game = new SuperContraGame();
+		
+		//GamingConsole game = new MarioGame();
+		//GameRunner runner = new GameRunner(game);
+		
+		GameRunner runner = context.getBean(GameRunner.class);
+		
+		runner.run();
 	}
 
 }
---
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/game/PacManGame.java
@@ -0,0 +1,22 @@
+package com.in28minutes.spring.learnspringframework.game;
+
+import org.springframework.stereotype.Component;
+
+public class PacManGame implements GamingConsole{
+
+	public void up() {
+		System.out.println("PacMan up");
+	}
+	
+	public void down() {
+		System.out.println("PacMan down");
+	}
+	
+	public void left() {
+		System.out.println("PacMan left");
+	}
+	
+	public void right() {
+		System.out.println("PacMan right");
+	}
+}
